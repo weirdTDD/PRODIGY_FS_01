6781d8fb676e228e6ef1c71fd65546791e7cd34c
Edward Morhe <edwardmorhe777@gmail.com>
2026-01-12 00:04:21 +0000

    new user model defined

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,45 @@
+
+import Login from './components/Login'
+import Home from './components/Home'
+import Register from './components/Register'
+import Navbar from './components/Navbar'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import About from './components/About'
+
+function App() {
+
+  const location = useLocation();
+  const noNavbar = location.pathname === '/register' || location.pathname === '/'
+  
+
+  return (
+    <>
+      {noNavbar ? 
+        <Routes>
+          <Route path = "/" element = {<Login/>}/>
+          <Route path = "/register" element={<Register/>}/>
+        </Routes>
+
+        :
+        
+        <Navbar 
+          //the contents are wrapped in the navbar and its contents 
+          content={
+            <Routes>
+              <Route path= "/home" element = {<Home/>} />
+              <Route path= "/about" element = {<About/>} />
+              
+            </Routes>
+          }
+        />
+      
+      }
+    
+      
+      
+      
+    </>
+  )
+}
+
+export default App
---
+++ frontend/frontend/src/components/Register.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>Please Register Here</div>
+  )
+}
+
+export default Register
